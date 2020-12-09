817f1d830ac8b6557e378f60a65df7345e142ec4
Fahim Al Islam <fahim6668@gmail.com>
2020-12-10 04:12:09 +0600

    Updated: added tinymce

--- canadasocceracademy/deploy/settings.py
+++ canadasocceracademy/deploy/settings.py
@@ -12,3 +12,5 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, 'static')
+
+TINYMCE_JS_URL = 'https://marvelous-tech.nyc3.cdn.digitaloceanspaces.com/tinymce/tinymce.min.js'
