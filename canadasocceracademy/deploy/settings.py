cbf0bc898d821a67055f7941d508028709eedbd2
Fahim Al Islam <fahim6668@gmail.com>
2020-12-10 01:35:07 +0600

    Updated: Diff++

--- canadasocceracademy/deploy/settings.py
+++ canadasocceracademy/deploy/settings.py
@@ -4,6 +4,6 @@ import dj_database_url
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, 'static')
+STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, 'static')
