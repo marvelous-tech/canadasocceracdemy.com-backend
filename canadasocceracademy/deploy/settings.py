371fb3039b332baaa6d3d0fd266efdd1aa51eef1
Fahim Al Islam <fahim6668@gmail.com>
2021-01-12 22:55:21 +0600

    Updated: added more padding

--- canadasocceracademy/deploy/settings.py
+++ canadasocceracademy/deploy/settings.py
@@ -12,5 +12,3 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATIC_URL = 'https://cdn.jsdelivr.net/gh/marvelous-tech/canadasocceracdemy.com-backend@master/staticfiles/'
-
-TINYMCE_JS_URL = 'https://marvelous-tech.nyc3.cdn.digitaloceanspaces.com/tinymce/tinymce.min.js'
