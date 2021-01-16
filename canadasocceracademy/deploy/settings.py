135f9cf4778a4a0ee3f390932172bcbde0b0a47c
Fahim Al Islam <fahim6668@gmail.com>
2021-01-16 23:16:35 +0600

    Updated: fixed static url in development mode and Review #6

--- canadasocceracademy/deploy/settings.py
+++ canadasocceracademy/deploy/settings.py
@@ -3,10 +3,6 @@ import dj_database_url
 
 from canadasocceracademy.settings import *
 
-
-
-
-
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
