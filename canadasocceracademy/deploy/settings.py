5c11eb4ea033d9548e98d2d0d724174f8c2c1986
Fahim Al Islam <fahim6668@gmail.com>
2021-01-18 00:58:40 +0600

    Updated: migrated to V:1.1.34

--- canadasocceracademy/deploy/settings.py
+++ canadasocceracademy/deploy/settings.py
@@ -5,6 +5,6 @@ from canadasocceracademy.settings import *
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_1-1-33/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_1-1-34/')
 
-STATIC_URL = 'https://cdn.jsdelivr.net/gh/marvelous-tech/canadasocceracdemy.com-backend@master/staticfiles_1-1-33/'
+STATIC_URL = 'https://cdn.jsdelivr.net/gh/marvelous-tech/canadasocceracdemy.com-backend@master/staticfiles_1-1-34/'
