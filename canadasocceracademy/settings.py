2d75b89ab89e60c33abd9762a7f78098d5aa79b4
Fahim Al Islam <fahim6668@gmail.com>
2021-01-24 03:36:12 +0600

    Updated: added timezone Canada/Central for admin portal

--- canadasocceracademy/settings.py
+++ canadasocceracademy/settings.py
@@ -106,6 +106,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'core.middlewares.TimezoneMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'qinspect.middleware.QueryInspectMiddleware',
--- core/middlewares.py
+++ core/middlewares.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponsePermanentRedirect
+import pytz
+from django.utils import timezone
 
 
 class WwwRedirectMiddleware:
@@ -13,3 +15,17 @@ class WwwRedirectMiddleware:
             )
         else:
             return self.get_response(request)
+
+
+class TimezoneMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        tzname = 'Canada/Central'
+        if tzname:
+            if request.user.is_staff():
+                timezone.activate(pytz.timezone(tzname))
+        else:
+            timezone.deactivate()
+        return self.get_response(request)
